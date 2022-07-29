388b669cf075712497c67cbfb4edf9aa54156912
MuadDAtr <int.stron@protonmail.com>
2022-07-30 00:21:32 +0200

    adding input

--- quiz.py
+++ quiz.py
@@ -1,5 +1,7 @@
 from json import load
 
+points_counter = 0
+
 with open('quiz.json') as quest_file:
     questions = load(quest_file)
     #print(questions)
@@ -7,4 +9,12 @@ with open('quiz.json') as quest_file:
     for question in questions:
         print(question['question'])
         print("")
-        print('~~~' *20)
+        for index, answer in enumerate(question['answers'], 1):
+            print(index, answer['ans'])
+        print("")
+        
+        users_answer = int(input("What's your answer?  "))
+        print("")
+        print('~~~'*20)
+
+    
